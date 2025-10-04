59327901ee71d084859c1210d276538b595a786d
yogesh khangode <yogeshkhagode.99@gmail.com>
2025-10-04 20:28:05 +0530

    add new changes

--- dataos-event-data-product/event-360/build/semantic-model/event-360/model/sqls/clickstream.sql
+++ dataos-event-data-product/event-360/build/semantic-model/event-360/model/sqls/clickstream.sql
@@ -3,7 +3,7 @@
     session_id,
     app_name,
     date(from_unixtime(timestamp / 1000)) AS event_date,
-    cast(date_diff('second', min(from_unixtime(timestamp / 1000)), max(from_unixtime(timestamp / 1000))) AS decimal(38,0)) AS session_duration_seconds
+    cast(date_diff('second', min(from_unixtime(timestamp / 1000)), max(from_unixtime(timestamp / 1000))) AS decimal(12,2)) AS session_duration_seconds
   FROM
     "icebase"."sys01".page_events_01
     group by 1,2,3,4
