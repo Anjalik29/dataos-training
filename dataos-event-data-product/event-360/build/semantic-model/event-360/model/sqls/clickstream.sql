84210a566920dd4dc72fd31d8d645abc8c3801c4
yogesh khangode <yogeshkhagode.99@gmail.com>
2025-10-04 20:10:25 +0530

    add new changes in sql

--- dataos-event-data-product/event-360/build/semantic-model/event-360/model/sqls/clickstream.sql
+++ dataos-event-data-product/event-360/build/semantic-model/event-360/model/sqls/clickstream.sql
@@ -1,10 +1,10 @@
-SELECT
-  event_id,
-  event_type,
-  event_name,
-  date(FROM_UNIXTIME("timestamp" / 1000)) as event_date,
-  user_id,
-  session_id,
-  app_name
-FROM
-  icebase.sys01.page_events_01
+ SELECT
+    event_id,
+    user_id,
+    session_id,
+    app_name,
+    date(from_unixtime(timestamp / 1000)) AS event_date,
+    date_diff('second', min(from_unixtime(timestamp / 1000)), max(from_unixtime(timestamp / 1000))) AS session_duration_seconds
+  FROM
+    "icebase"."sys01".page_events_01
+    group by 1,2,3,4,5
